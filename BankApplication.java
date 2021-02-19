eb1a92ada2ed118ef36e30fabf57aef352af0726
Whizma <isak@maatta.se>
2021-02-19 15:55:35 +0100

    ass

--- BankApplication.java
+++ BankApplication.java
@@ -1,9 +1,19 @@
-
+// System.out och scanner 
 public class BankApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Customer cust = new Customer("Diego Digge",69042069);
+		BankAccount ba = new BankAccount("Diego Digge", 69042069);
 
 	}
+	private static void runApplication();
+	1. Hitta konto utifrån innehavare
+	2. Sök kontoinnehavare utifrån (del av) namn
+	3. Sätt in
+	4. Ta ut
+	5. Överföring
+	6. Skapa konto
+	7. Ta bort konto
+	8. Skriv ut konton
 
 }
